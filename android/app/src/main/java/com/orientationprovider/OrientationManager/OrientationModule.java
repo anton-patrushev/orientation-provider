8aeec022f77f5838a73bcc76cd7d74f880b8d3f4
phantom-patrick <anton11patrushev@gmail.com>
2021-02-14 18:15:42 +0300

    create & register Android native module

---
+++ android/app/src/main/java/com/orientationprovider/OrientationManager/OrientationModule.java
@@ -0,0 +1,23 @@
+package com.orientationprovider.OrientationManager;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class OrientationModule extends ReactContextBaseJavaModule {
+    OrientationModule(ReactApplicationContext context) {
+        super(context);
+    }
+
+    @Override
+    public String getName() {
+        return "OrientationManager";
+    }
+}
